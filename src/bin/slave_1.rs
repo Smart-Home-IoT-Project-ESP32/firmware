98cf264c7009dfc47baaf3665881c81fd7e58771
Leonardo Danelutti <leodane2002@gmail.com>
2024-06-11 23:20:09 +0200

    Doc for slave1

--- src/bin/slave_1.rs
+++ src/bin/slave_1.rs
@@ -40,6 +40,7 @@ fn main() {
     // Create a WifiDriver instance.
     let mut wifi_driver = WifiDriver::new(peripherals.modem, sys_loop, Some(nvs)).unwrap();
 
+    // Set the Wi-Fi configuration as a client
     wifi_driver.set_configuration(&Configuration::Client(ClientConfiguration::default())).unwrap();
 
     // To avoid this issue: https://github.com/espressif/esp-idf/issues/10341
@@ -63,12 +64,12 @@ fn main() {
     let esp_now = EspNow::take().unwrap();
 
     // Create a channel to communicate between threads
-
     let (sender, reciver) = std::sync::mpsc::sync_channel(10);
 
-    // register reciving callback
+    // register reciving callback, this is used to add the master board to the peer list
     esp_now.register_recv_cb(|mac_address, _data| {
         let mac_address_array = mac_address.try_into().unwrap();
+        // If peer does not exist, add it
         if let Ok(false) = esp_now.peer_exists(mac_address_array) {
             // Add the peer
             let peer = PeerInfo {
@@ -79,6 +80,7 @@ fn main() {
         }
     }).unwrap();
 
+    // Create a thread to read the dhtt sensor
     let sender_dhtt = sender.clone();
 
     std::thread::spawn(move || {
@@ -87,26 +89,32 @@ fn main() {
                 // convert the reading to a message
                 let message_temp = TemperatureMessage::new().with_temperature(reading.temperature.try_into().unwrap());
                 let frame: Frame = message_temp.into();
+                // send it to the main task
                 sender_dhtt.send(frame).unwrap();
                 let message_hum = HumidityMessage::new().with_humidity(reading.relative_humidity.try_into().unwrap());
                 let frame: Frame = message_hum.into();
                 sender_dhtt.send(frame).unwrap();       
             }
-            std::thread::sleep(std::time::Duration::from_secs(5));
+            std::thread::sleep(std::time::Duration::from_secs(10));
         }
     });
 
+    // Create a thread to read the flame sensor
     std::thread::spawn(move || {
         loop {
             let flame = flame_pin.is_high();
             let message = FireAlarmMessage::new().with_fire_alarm(flame);
+            // send it to the main task
             sender.send(message.into()).unwrap();
-            std::thread::sleep(std::time::Duration::from_secs(5));
+            std::thread::sleep(std::time::Duration::from_secs(15));
         }
     });
 
+    // Main task
     loop {
+        // Wait for a message
         let frame_to_send = reciver.recv().unwrap();
+        // If the peer exists, send the message
         if let Ok(peer) = esp_now.fetch_peer(true) {
             esp_now.send(peer.peer_addr, &frame_to_send.serialize()).unwrap();
         }
