a0965c967e4bb8a165367d94be7f2c0f269fe75c
Gaia Simeoni <87936039+g4ix@users.noreply.github.com>
2024-06-13 12:11:09 +0200

    AP creation and server page.

---
+++ examples/http_server.rs
@@ -0,0 +1,158 @@
+//! HTTP Server with JSON POST handler
+//!
+//! Go to 192.168.71.1 to test
+
+use core::convert::TryInto;
+
+use embedded_svc::{
+    http::{Headers, Method},
+    io::{Read, Write},
+    wifi::{self, AccessPointConfiguration},
+};
+
+use esp_idf_svc::{
+    eventloop::EspSystemEventLoop,
+    http::server::EspHttpServer,
+    nvs::EspDefaultNvsPartition,
+    wifi::{BlockingWifi, EspWifi},
+};
+use esp_idf_svc::{hal::prelude::Peripherals, wifi::ClientConfiguration};
+
+use log::*;
+
+use serde::Deserialize;
+
+static INDEX_HTML: &str = include_str!("http_server_page.html");
+
+// Max payload length
+const MAX_LEN: usize = 128;
+// Need lots of stack to parse JSON
+const STACK_SIZE: usize = 10240;
+
+#[derive(Deserialize)]
+struct FormData<'a> {
+    wifi_ssid: &'a str,
+    wifi_pass: &'a str,
+}
+
+impl<'a> std::fmt::Display for FormData<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Wi-Fi SSID: {}, Password: {}", self.wifi_ssid, self.wifi_pass)
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    esp_idf_svc::sys::link_patches();
+    esp_idf_svc::log::EspLogger::initialize_default();
+    let mut server = create_server()?;
+
+
+    server.fn_handler("/", Method::Get, |req| {
+        req.into_ok_response()?
+            .write_all(INDEX_HTML.as_bytes())
+            .map(|_| ())
+    })?;
+
+
+    server.fn_handler::<anyhow::Error, _>("/post", Method::Post, |mut req| {
+        let len = req.content_len().unwrap_or(0) as usize;
+
+        if len > MAX_LEN {
+            req.into_status_response(413)?
+                .write_all("Request too big".as_bytes())?;
+            return Ok(());
+        }
+
+
+        let mut buf = vec![0; len];
+        req.read_exact(&mut buf)?;
+        let mut resp = req.into_ok_response()?;
+
+
+        if let Ok(form) = serde_json::from_slice::<FormData>(&buf) {
+
+            info!(
+                "Wi-Fi SSID: {}, Password: {}\n",
+                form.wifi_ssid,
+                form.wifi_pass
+            );
+
+            let peripherals = Peripherals::take()?;
+            let sys_loop = EspSystemEventLoop::take()?;
+            let nvs = EspDefaultNvsPartition::take()?;
+            
+            let mut wifi = BlockingWifi::wrap(
+                EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs))?,
+                sys_loop,
+            )?;
+
+            let client_configuration = wifi::Configuration::Client(ClientConfiguration {
+                ssid: form.wifi_ssid.try_into().unwrap(),
+                password: form.wifi_pass.try_into().unwrap(),
+                ..Default::default()
+            });
+        
+        
+            wifi.set_configuration(&client_configuration)?;
+        
+        
+            wifi.start().unwrap();
+            wifi.connect().unwrap();
+            info!("Connected to Wi-Fi");
+        
+            wifi.wait_netif_up().unwrap();        
+            
+        } else {
+            resp.write_all("JSON error".as_bytes())?;
+        }
+
+        Ok(())
+    })?;
+
+    // Keep server running beyond when main() returns (forever)
+    // Do not call this if you ever want to stop or access it later.
+    // Otherwise you can either add an infinite loop so the main task
+    // never returns, or you can move it to another thread.
+    // https://doc.rust-lang.org/stable/core/mem/fn.forget.html
+    core::mem::forget(server);
+
+    // Main task no longer needed, free up some memory
+    Ok(())
+}
+
+fn create_server() -> anyhow::Result<EspHttpServer<'static>> {
+    let peripherals = Peripherals::take()?;
+    let sys_loop = EspSystemEventLoop::take()?;
+    let nvs = EspDefaultNvsPartition::take()?;
+
+    let mut wifi = BlockingWifi::wrap(
+        EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs))?,
+        sys_loop,
+    )?;
+
+    let wifi_configuration = wifi::Configuration::AccessPoint(AccessPointConfiguration {
+        ssid: "ESP32-Access-Point".try_into().unwrap(),
+        ssid_hidden: false,
+        channel: 0,
+        ..Default::default()
+    });
+    wifi.set_configuration(&wifi_configuration)?;
+    wifi.start()?;
+    wifi.wait_netif_up()?;
+
+    info!("Created AP");
+
+    let server_configuration = esp_idf_svc::http::server::Configuration {
+        stack_size: STACK_SIZE,
+        ..Default::default()
+    };
+
+    // Keep wifi running beyond when this function returns (forever)
+    // Do not call this if you ever want to stop or access it later.
+    // Otherwise it should be returned from this function and kept somewhere
+    // so it does not go out of scope.
+    // https://doc.rust-lang.org/stable/core/mem/fn.forget.html
+    core::mem::forget(wifi);
+
+    Ok(EspHttpServer::new(&server_configuration)?)
+}
--- src/bin/slave_2.rs
+++ src/bin/slave_2.rs
@@ -1,11 +1,20 @@
 // Slave that is used to read data from LM35 sensor and Gas sensor and send it to the master.
+use core::sync::atomic::AtomicBool;
+use core::sync::atomic::Ordering;
+
 use embedded_svc::wifi::ClientConfiguration;
 use embedded_svc::wifi::Configuration;
+
 use esp_idf_hal::adc::config::Resolution::Resolution12Bit;
 use esp_idf_hal::adc::*;
+use esp_idf_hal::delay::BLOCK;
 use esp_idf_hal::gpio::Gpio10;
 use esp_idf_hal::gpio::Gpio11;
 use esp_idf_hal::peripherals::Peripherals;
+
+use esp_idf_hal::task::notification::Notification;
+use esp_idf_hal::task::wait_notification;
+use esp_idf_svc::espnow::SendStatus;
 use esp_idf_svc::espnow::{EspNow, PeerInfo};
 use esp_idf_svc::eventloop::EspSystemEventLoop;
 use esp_idf_svc::nvs::EspDefaultNvsPartition;
@@ -14,13 +23,15 @@ use esp_idf_svc::sys::{
     WIFI_PROTOCOL_11N, WIFI_PROTOCOL_LR,
 };
 use esp_idf_svc::wifi::WifiDriver;
+
+use firmware::utilities::channel::set_channel;
 use firmware::GasLeakageMessage;
 use firmware::TemperatureMessage;
 use messages::Frame;
 use std::thread;
 use std::time::Duration;
 
-const WIFI_CHANNEL: u8 = 1;
+static IS_SEARCHING_CHANNEL: AtomicBool = AtomicBool::new(true);
 
 fn main() {
     // Init
@@ -50,7 +61,6 @@ fn main() {
     let mut gas_pin: AdcChannelDriver<'_, { attenuation::DB_11 }, Gpio11> =
         AdcChannelDriver::new(peripherals.pins.gpio11).unwrap();
 
-
     // ------------------------------ //
     //            ESP-NOW             //
     // ------------------------------ //
@@ -62,16 +72,10 @@ fn main() {
     // Create a WifiDriver instance.
     let mut wifi_driver = WifiDriver::new(peripherals.modem, sys_loop, Some(nvs)).unwrap();
 
-    wifi_driver.set_configuration(&Configuration::Client(ClientConfiguration::default()));
-
-    // To avoid this issue: https://github.com/espressif/esp-idf/issues/10341
-    unsafe {
-        esp_idf_hal::sys::esp_wifi_set_promiscuous(true);
-        let channel = WIFI_CHANNEL;
-        let second = esp_idf_hal::sys::wifi_second_chan_t_WIFI_SECOND_CHAN_NONE;
-        esp_idf_hal::sys::esp_wifi_set_channel(channel, second);
-        esp_idf_hal::sys::esp_wifi_set_promiscuous(false);
-    }
+    // Set the Wi-Fi configuration as a client
+    wifi_driver
+        .set_configuration(&Configuration::Client(ClientConfiguration::default()))
+        .unwrap();
 
     // Set protocol to accept Long range also
     unsafe {
@@ -89,12 +93,12 @@ fn main() {
 
     // Create a channel to communicate between threads
     let (sender, reciver) = std::sync::mpsc::sync_channel(10);
+    // Notification to search for a master board on other channels
+    let channel_search = Notification::new();
+    let channel_search_notifier = channel_search.notifier();
 
-    // Clone the sender to be used in two threads
-    let sender_clone = sender.clone();
-
-    // register reciving callback
-    esp_now.register_recv_cb(|mac_address, data| {
+    // register reciving callback, this is used to add master board to the peer list
+    esp_now.register_recv_cb(|mac_address, _data| {
         let mac_address_array = mac_address.try_into().unwrap();
         if let Ok(false) = esp_now.peer_exists(mac_address_array) {
             // Add the peer
@@ -104,13 +108,36 @@ fn main() {
             };
             esp_now.add_peer(peer).unwrap();
         }
-    });
+    }).unwrap();
+
+    // Register the send callback, this is used to detect if the master is not reachable
+    let mut num_fail: usize = 0;
+    esp_now
+        .register_send_cb(|_mac_addres, status| {
+            // if a send fails for more than 10 times, start searching for the master board on other channels
+            if let SendStatus::SUCCESS = status {
+                num_fail = 0;
+                IS_SEARCHING_CHANNEL.store(false, Ordering::Relaxed)
+            } else {
+                num_fail = num_fail.checked_add(1).unwrap_or(0);
+            }
+
+            if num_fail > 10 {
+                // Safty: we don't call mem::forget on the Notification
+                unsafe {
+                    channel_search_notifier.notify(core::num::NonZeroU32::new(1).unwrap());
+                }
+                IS_SEARCHING_CHANNEL.store(true, Ordering::Relaxed);
+            }
+        })
+        .unwrap();
 
     // ------------------------------ //
     //            Threads             //
     // ------------------------------ //
 
     // Create a thread to read the lm35 sensor
+    let sender_lm35 = sender.clone();
     std::thread::spawn(move || loop {
         // Read the data from the lm35 sensor using ADC
         let lm35_raw_data = adc_1.read(&mut lm35_temp_pin).unwrap();
@@ -120,7 +147,7 @@ fn main() {
         let message_temp =
             TemperatureMessage::new().with_temperature(lm35_preprocessed_data.try_into().unwrap());
         let frame: Frame = message_temp.into();
-        sender.send(frame);
+        sender_lm35.send(frame).unwrap();
 
         thread::sleep(Duration::from_secs(5));
     });
@@ -132,8 +159,23 @@ fn main() {
         // Create a message with the gas data and send it to the main task
         let gas_message = GasLeakageMessage::new().with_gas_leakage(gas_data.try_into().unwrap());
         let frame: Frame = gas_message.into();
-        sender_clone.send(frame);
+        sender.send(frame).unwrap();
         thread::sleep(Duration::from_secs(5));
+    });
+
+    // Scan for an evailable channel
+    std::thread::spawn(move || {
+        let mut channel = 6;
+        loop {
+            while IS_SEARCHING_CHANNEL.load(Ordering::Relaxed) {
+                set_channel(channel);
+                // channels 1, 6 and 11 are the most common channels
+                channel = (channel + 5) % 15;
+                std::thread::sleep(Duration::from_secs(2));
+            }
+            // channel found, wait untill a notification is received
+            wait_notification(BLOCK);
+        }
     });
 
     // Main task
@@ -150,19 +192,19 @@ fn main() {
 }
 
 /// Convert the raw data from the LM35 sensor to temperature.
-/// 
-/// The `voltage` is calculated by: 
+///
+/// The `voltage` is calculated by:
 /// * multyplying the raw data 3.1, which is the attenuation of the ADC in Volts
 /// * and dividing it by 4095, which is the number of bits of the ADC [2^12-1].
-/// 
-/// The `temperature` is calculated by multiplying the `voltage` by 100, 
+///
+/// The `temperature` is calculated by multiplying the `voltage` by 100,
 /// which is the temperature in Celsius.
-/// 
-/// 
+///
+///
 /// # Arguments
-/// 
+///
 /// * `raw_data` - The raw data from the LM35 sensor.
-/// 
+///
 pub fn convert_lm35_data(raw_data: u16) -> f32 {
     let voltage = raw_data as f32 * 3.1 / 4095.0;
     let temperature = voltage * 100.0;
